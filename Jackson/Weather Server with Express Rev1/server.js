1b1edebbadc98346b14e6a84e7f6c68533575919
Jackson Moore <JacksonMoore012@gmail.com>
2017-11-19 16:11:00 -0500

    Added weather tutorial and weather express server example

---
+++ Jackson/Weather Server with Express Rev1/server.js
@@ -0,0 +1,51 @@
+/*
+NOTE: THIS CODE WILL NOT RUN UNTIL YOU ENTER YOUR OWN openweathermap.org APP ID KEY
+
+NOTE: You need to intall the npm modules by executing >npm install
+before running this server
+
+Simple express server re-serving data from openweathermap.org
+To test: 
+http://localhost:3000
+or
+http://localhost:3000/weather?city=Ottawa
+to just set JSON response. (Note it is helpful to add a JSON formatter extension, like JSON Formatter, to your Chrome browser for viewing just JSON data.)
+*/
+const express = require('express') //express framework
+const requestModule = require('request') //npm module for easy http requests
+const PORT = process.env.PORT || 3000
+
+/*YOU NEED AN APP ID KEY TO RUN THIS CODE
+  GET ONE BY SIGNING UP AT openweathermap.org
+  THE KEY BELOW IS FAKE
+*/
+const WEATHER_API_KEY = '2753e02fbb7691461a7fe6ed88806719' //PUT IN YOUR OWN KEY HERE
+
+const app = express()
+
+//Middleware
+app.use(express.static(__dirname + '/public')) //static server
+
+//Routes
+app.get('/', (request, response) => {
+  response.sendFile(__dirname + '/views/index.html')
+})
+
+app.get('/weather', (request, response) => {
+  let city = request.query.city
+  if(!city) {
+    return response.json({message: 'Please enter a city name'})
+  }
+  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${WEATHER_API_KEY}`
+  requestModule.get(url, (err, res, data) => {
+    return response.contentType('application/json').json(JSON.parse(data))
+  })
+})
+
+//start server
+app.listen(PORT, err => {
+  if(err) console.log(err)
+  else {
+    console.log(`Server listening on port: ${PORT}`)
+  }
+})
